ef0fe75a00aaf7c1cdb3ffc2a0d936f624e12abe
Kamaal ABOOTHALIB <kamaal@kamaal.me>
2025-01-08 09:49:17 +0100

    chore: export more components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import SuccessSvg from 'components/fallback-components/success-svg';
+import ErrorSvg from 'components/fallback-components/error-svg';
+import Animated from 'components/animated';
+import ClockSvg from 'components/fallback-components/clock-svg';
+
 import { useThumbnailsGenerator } from './hook/use-thumbnails-generator';
 import Thumb from './components/thumb';
 import ThumbMin from './components/tailwind/thumb-min';
 import InfoDisplay from './components/info-display';
+import CancelButton from './components/fallback-components/cancel-button';
 
 export * from './@types/';
-export { useThumbnailsGenerator, Thumb, InfoDisplay, ThumbMin };
+export { useThumbnailsGenerator, Thumb, InfoDisplay, ThumbMin, CancelButton, SuccessSvg, ErrorSvg, Animated, ClockSvg };
